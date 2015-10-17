e829afbee0fef500e9ed6cbf0a95d0819877802d
Yifei LIU <yfliu87@gmail.com>
2015-10-17 13:42:51 +0800

    derive sentiment of new terms

--- Systems_and_Algorithms/assignment1/term_sentiment.py
+++ Systems_and_Algorithms/assignment1/term_sentiment.py
@@ -1,17 +1,76 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
+def read_sent(sent_file):
+	reader = open(sent_file)
+	sent_dict = {}
 
-def lines(fp):
-    print str(len(fp.readlines()))
+	for line in reader:
+		term, score = line.split('\t')
+
+		if ' ' in term:
+			subs = term.split(' ')
+
+			for sub in subs:
+				sent_dict[sub] = score
+		else:
+			sent_dict[term] = score
+
+	reader.close()
+	return sent_dict
+
+
+def parse_tweet(tweet_file, sent_dict):
+	reader = open(tweet_file, 'r')
+
+	for line in reader:
+		json_line = json.loads(line)
+
+		if json_line.has_key('delete'):
+			continue
+
+		text = json_line['text']
+		overall, new_words = calculate_overall_sentiment(text.encode('utf-8'), sent_dict)
+
+		for word in new_words:
+			sent_dict[word] = float(overall)
+
+	reader.close()
+
+	for word, score in sent_dict.items():
+		print str(word) + ' ' + str(float(score))
+
+
+def calculate_overall_sentiment(text, sent_dict):
+	overall = 0.0
+	new_words = []
+
+	text = text.lower()
+	splitor = [',','.','...','/','!','@','?','$','%','*','&','#','~',':',';','(',')']
+
+	for sp in splitor:
+		if sp in text:
+			text = text.replace(sp, ' ')
+
+	words = text.split(' ')
+
+	for word in words:
+		if not word:
+			continue
+
+		if word not in sent_dict:
+			new_words.append(word)
+		else:
+			overall += float(sent_dict[word])
+
+	return overall, new_words
+
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    sent_file = sys.argv[1]
+    tweet_file = sys.argv[2]
+    sent_dict = read_sent(sent_file)
+    parse_tweet(tweet_file, sent_dict)
 
 if __name__ == '__main__':
     main()
