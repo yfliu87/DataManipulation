2081838cff15f1ca291a27d197fefb2e8f824c4c
Yifei LIU <yfliu87@gmail.com>
2015-10-17 10:41:36 +0800

    parse tweets by sentiment

--- Systems_and_Algorithms/assignment1/tweet_sentiment.py
+++ Systems_and_Algorithms/assignment1/tweet_sentiment.py
@@ -12,12 +12,34 @@ def read_sent_file(sent_file):
 	reader.close()
 	return sent_dict
 
+
+def sent_score(tweet_file, sent_dict):
+	import json
+	reader = open(tweet_file, 'r')
+
+	for line in reader:
+		json_line = json.loads(line)
+
+		if json_line.has_key('delete'):
+			print '0.0'
+			continue
+
+		text = json_line['text']
+		words = text.split(' ')
+
+		score = 0.0
+		for word in words:
+			if word in sent_dict:
+				score += float(sent_dict[word])
+
+		print str(score)
+
+
 def main():
     sent_file = sys.argv[1]
     tweet_file = sys.argv[2]
     sent_dict = read_sent_file(sent_file)
-    print len(sent_dict)
-    #sent_score(tweet_file, sent_dict)
+    sent_score(tweet_file, sent_dict)
 
 if __name__ == '__main__':
     main()
